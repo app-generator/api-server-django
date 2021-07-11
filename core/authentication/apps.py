d60d5fddd2d325c690daf16c85cc90325a3a853e
koladev <onaelmangabo@gmail.com>
2021-07-11 03:39:43 +0100

    - feat: add authentication models

--- core/authentication/apps.py
+++ core/authentication/apps.py
@@ -3,4 +3,6 @@ from django.apps import AppConfig
 
 class AuthenticationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'authentication'
+    name = 'core.authentication'
+    label = 'core_authentication'
+
