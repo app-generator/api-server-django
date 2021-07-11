4c07545aa2ef6e56e5f7f13ea23f32f5aba3098b
koladev <onaelmangabo@gmail.com>
2021-07-11 03:08:08 +0100

    - feat: add user viewsets

---
+++ core/user/viewsets.py
@@ -0,0 +1,45 @@
+from core.user.serializers import UserSerializer
+from core.user.models import User
+from rest_framework import viewsets, filters, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    http_method_names = ['get', 'put']
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['updated']
+    ordering = ['-updated']
+
+    def update(self, request, *args, **kwargs):
+        return NotImplementedError
+
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return User.objects.all()
+
+    def get_object(self):
+        lookup_field_value = self.kwargs[self.lookup_field]
+
+        obj = User.objects.get(lookup_field_value)
+        self.check_object_permissions(self.request, obj)
+
+        return obj
+
+    @action(detail=True, methods=['put'])
+    def edit(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
