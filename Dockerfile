fe47719a40b6444d470ee66dceef8c8812771055
koladev <onaelmangabo@gmail.com>
2021-07-14 16:07:41 +0100

    - chore: swithc to sqlite

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
 RUN apt-get update \
-  && apt-get -y install netcat gcc postgresql \
+  && apt-get -y install netcat gcc \
   && apt-get clean
 
 RUN pip install --upgrade pip
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
-
-    while ! nc -z $DB_HOST $DB_PORT; do
-      sleep 0.1
-    done
-
-    echo "PostgreSQL started"
-fi
-
 python manage.py flush --no-input
 python manage.py migrate
 
