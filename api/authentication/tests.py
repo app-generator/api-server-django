ab1961e22f8b9618df27a6cb4b8619efc8237797
koladev <onaelmangabo@gmail.com>
2021-07-15 13:19:38 +0100

    - test: add registration test

--- api/authentication/tests.py
+++ api/authentication/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+
+class FilmViewsTest(APITestCase):
+    base_url = reverse("api:register-list")
+
+    data = {
+        "username": "coucou",
+        "password": "12345678",
+        "email": "teast@admin.com"
+    }
+
+    def setUp(self) -> None:
+        pass
+
+    def test_list(self):
+        response = self.client.post(f"{self.base_url}", data=self.data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
