f3f6cb7a8d55c9e1cd58f9c8186d9ae2bb156fa3
koladev <onaelmangabo@gmail.com>
2021-07-15 12:30:57 +0100

    - refactoring: remove trailing slash

--- api/routers.py
+++ api/routers.py
@@ -2,7 +2,8 @@ from api.authentication.viewsets.register import RegisterViewSet
 from rest_framework import routers
 from api.user.viewsets import UserViewSet
 
-router = routers.SimpleRouter()
+router = routers.SimpleRouter(trailing_slash=False)
+
 router.register(r'edit', UserViewSet, basename='users')
 
 router.register(r'register', RegisterViewSet, basename='register')
