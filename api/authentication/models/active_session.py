2ea8c12a8c1d805ea46fd39fe9043b3ebef914d1
koladev <onaelmangabo@gmail.com>
2021-07-15 12:31:28 +0100

    - fix: modify models foreign key

--- api/authentication/models/active_session.py
+++ api/authentication/models/active_session.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class ActiveSession(models.Model):
-    user = models.ForeignKey("core_user.User", on_delete=models.CASCADE)
+    user = models.ForeignKey("api_user.User", on_delete=models.CASCADE)
     token = models.CharField(max_length=255)
     date = models.DateTimeField(auto_now_add=True)
