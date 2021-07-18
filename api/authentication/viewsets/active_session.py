4a25d2b0cf1d0eb2258d953d1b6b74fd601aae68
Kolawole <onaelmangabo@gmail.com>
2021-07-18 03:22:34 +0200

    - feat: add check session endpoint

--- api/authentication/viewsets/active_session.py
+++ api/authentication/viewsets/active_session.py
@@ -1,11 +1,13 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins, status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.permissions import AllowAny, IsAuthenticated
 
 
-class ActiveSessionViewSet(viewsets.ModelViewSet):
+class ActiveSessionViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin):
     http_method_names = ['post']
+    permission_classes = (IsAuthenticated,)
 
     def create(self, request, *args, **kwargs):
-        pass
+        return Response({
+            "success": True
+        }, status.HTTP_200_OK)
--- api/routers.py
+++ api/routers.py
@@ -1,4 +1,4 @@
-from api.authentication.viewsets import RegisterViewSet, LoginViewSet
+from api.authentication.viewsets import RegisterViewSet, LoginViewSet, ActiveSessionViewSet
 from rest_framework import routers
 from api.user.viewsets import UserViewSet
 
@@ -9,6 +9,8 @@ router.register(r'edit', UserViewSet, basename='users')
 router.register(r'register', RegisterViewSet, basename='register')
 
 router.register(r'login', LoginViewSet, basename='login')
+
+router.register(r'checkSession', ActiveSessionViewSet, basename='check-session')
 
 urlpatterns = [
     *router.urls,
