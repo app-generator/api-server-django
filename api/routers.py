204cca4214a7b8ab6c64f7aa6fc6b6a10a3e03fa
Kolawole <onaelmangabo@gmail.com>
2021-07-18 03:36:02 +0200

    - feat: add logout endpoint

--- api/routers.py
+++ api/routers.py
@@ -1,4 +1,4 @@
-from api.authentication.viewsets import RegisterViewSet, LoginViewSet, ActiveSessionViewSet
+from api.authentication.viewsets import RegisterViewSet, LoginViewSet, ActiveSessionViewSet, LogoutViewSet
 from rest_framework import routers
 from api.user.viewsets import UserViewSet
 
@@ -11,6 +11,8 @@ router.register(r'register', RegisterViewSet, basename='register')
 router.register(r'login', LoginViewSet, basename='login')
 
 router.register(r'checkSession', ActiveSessionViewSet, basename='check-session')
+
+router.register(r'logout', LogoutViewSet, basename='logout')
 
 urlpatterns = [
     *router.urls,
