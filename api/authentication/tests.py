8b732db593e92ab656f840a0101076a7c59b47db
koladev <onaelmangabo@gmail.com>
2021-07-20 01:22:24 +0100

    - test: add test for check session / logout

--- api/authentication/tests.py
+++ api/authentication/tests.py
@@ -1,12 +1,13 @@
 from django.urls import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
-from api.fixtures import run_fixtures
 
 
 class AuthenticationTest(APITestCase):
     base_url_register = reverse("api:register-list")
     base_url_login = reverse("api:login-list")
+    base_url_logout = reverse("api:logout-list")
+    base_url_check_session = reverse("api:check-session-list")
 
     data_register = {
         "username": "test",
@@ -26,16 +27,43 @@ class AuthenticationTest(APITestCase):
         self.assertEqual(response_data['success'], True)
 
     def test_login(self):
-        # Running fixtures #
-        run_fixtures()
-
         response = self.client.post(f"{self.base_url_login}", data=self.data_login)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         response_data = response.json()
         self.assertEqual(response_data['success'], True)
 
     def test_logout(self):
-        pass
+        # Login to retrieve token
+
+        response = self.client.post(f"{self.base_url_login}", data=self.data_login)
+        response_data = response.json()
+
+        token = response_data['token']
+
+        self.client.credentials(HTTP_AUTHORIZATION=token)
+
+        # Logout
+
+        response = self.client.post(f"{self.base_url_logout}", data={
+            "token": token
+        })
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response_data = response.json()
+        self.assertEqual(response_data['success'], True)
 
     def test_check_session(self):
-        pass
+        # Login to retrieve token
+
+        response = self.client.post(f"{self.base_url_login}", data=self.data_login)
+        response_data = response.json()
+
+        token = response_data['token']
+
+        self.client.credentials(HTTP_AUTHORIZATION=token)
+
+        # Check session
+        response = self.client.post(f"{self.base_url_check_session}")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        response_data = response.json()
+        self.assertEqual(response_data['success'], True)
