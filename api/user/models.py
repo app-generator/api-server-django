a6e03e467c220cd5dd79f6b5bdf3e8a0521e29a6
koladev <onaelmangabo@gmail.com>
2022-06-07 23:44:25 +0100

    - feat: make username field unique for users

--- api/user/models.py
+++ api/user/models.py
@@ -41,7 +41,7 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    username = models.CharField(db_index=True, max_length=255)
+    username = models.CharField(db_index=True, max_length=255, unique=True)
     email = models.EmailField(db_index=True, unique=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
