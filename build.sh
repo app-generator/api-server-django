99140d187a43d4eeed71ee779fa3fab7742c2746
App Generator <51070104+app-generator@users.noreply.github.com>
2025-03-15 15:36:45 +0200

    CleanUP for Update

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-pip install --upgrade pip
-pip install -r requirements.txt
-
-python manage.py migrate
--- core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from django.contrib import admin
-from api.authentication.viewsets.social_login import GithubSocialLogin
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("api/users/", include(("api.routers", "api"), namespace="api")),
-    path("api/sessions/oauth/github/", GithubSocialLogin.as_view(), name="github_login"),
-]
